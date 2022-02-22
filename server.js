4d4261ace422980295cfae2e7381327eaaed8045
Yabko <o.yablinska@gmail.com>
2022-02-22 17:35:22 +0200

    dist fixes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ang-game-library'));
+app.use(express.static('./dist'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/ang-game-library/'}),
+    res.sendFile('index.html', {root: 'dist/'}),
 );
 
 // Start the app by listening on the default Heroku port
