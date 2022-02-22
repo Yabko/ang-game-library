cf04f44b59776b631bff361dd32c91724280ed32
Yabko <o.yablinska@gmail.com>
2022-02-22 16:11:05 +0200

    server minor typo fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/ang-game-library'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/ang-game-library/'}),
